783d917be07792a1dd7e3fe64155fcf4bfc35b96
marcos <ilejbr@gmail.com>
2022-11-16 12:10:21 +0000

    2 version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Tabuada de multiplicação. Escreva o número de 1 a 10:");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        for(int i = 0; i <= 10; ++i) {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
     }
 }
